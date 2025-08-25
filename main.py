cecfe0a42a0f6c50b773c1d20686b271910f913d
Bannu Jami <bhargavjami22@gmail.com>
2025-08-25 16:46:11 +0530

    All Errors are Fixed

--- main.py
+++ main.py
@@ -105,9 +105,4 @@ class WebCrawlerTests(unittest.TestCase):
         # Assert that the output was captured correctly by mock_stdout
 ###########################
 if __name__ == "__main__":
-    unittest.main()  # Run unit tests
-    main()  # Run your main application logic 
-
-
-if __name__ == "__main__":
     main()
